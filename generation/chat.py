6a011f3d4390ffa6de974dd6a260f7c1a195848d
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2024-03-14 20:13:17 +0100

    Refactor

--- generation/chat.py
+++ generation/chat.py
@@ -14,10 +14,10 @@ class ChatGenerator(GeneratorContract, AsyncService):
     Handles LLM powered chat responses.
     """
 
-    __history = Stack[ChatMessage](25, 1)
+    history = Stack[ChatMessage](25, 1)
 
     def __init__(self, _system_prompt: str = get_system_prompt()) -> None:
-        self.__history.push(ChatMessage(
+        self.history.push(ChatMessage(
             role=ChatRole.SYSTEM,
             content=_system_prompt
         ))
@@ -29,7 +29,7 @@ class ChatGenerator(GeneratorContract, AsyncService):
         self.set_busyness(True)
         start_stamp = datetime.now(UTC)
 
-        self.__history.push(ChatMessage(
+        self.history.push(ChatMessage(
             role=ChatRole.USER,
             content=_input.prompt
         ))
@@ -37,7 +37,7 @@ class ChatGenerator(GeneratorContract, AsyncService):
         async with aiohttp.ClientSession() as session:
             async with session.post(url=API_BASE + COMPLETION_ENDPOINT, json={
                     "model": "llama2-uncensored",
-                    "messages": self.chat_history.to_ollama_payload(),
+                    "messages": self.history.to_ollama_payload(),
                     "stream": False,
                 }) as generation_req:
                 gen_resp: dict = await generation_req.json()
@@ -45,7 +45,7 @@ class ChatGenerator(GeneratorContract, AsyncService):
         if "error" in gen_resp.keys():
             raise GenerationAPIException()
         
-        self.__history.push(ChatMessage.from_ollama_response(gen_resp))
+        self.history.push(ChatMessage.from_ollama_response(gen_resp))
 
         self.set_busyness(False)
         end_stamp = datetime.now(UTC)
@@ -54,6 +54,6 @@ class ChatGenerator(GeneratorContract, AsyncService):
             prompt=_input.prompt,
             model_name=_input.model_name,
             duration=(end_stamp - start_stamp),
-            data=self.__history.get_last().content,
+            data=self.history.get_last().content,
             extra=gen_resp
         )
