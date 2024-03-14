490a2508bb7a1f7d96908e1441483eb026a41411
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2024-03-14 01:53:55 +0100

    Update image model defs

--- generation/constants/diffusion_models.py
+++ generation/constants/diffusion_models.py
@@ -5,7 +5,7 @@ from ..types.structs import ImageModel, ImageSize
 
 # Optimized for speed, bad quality, unfiltered
 SD_15 = ImageModel(
-    id="sd15",
+    _id="sd15",
     dimensions=ImageSize(512, 512),
     model="sd_v1.5_f16.ckpt",
     guidance_scale=7,
@@ -16,7 +16,7 @@ SD_15 = ImageModel(
 
 # Balanced between speed and quality filtered
 DREAMSHAPER_XL_LIGHTNING = ImageModel(
-    id="dreamxl_lightning",
+    _id="dreamxl_lightning",
     dimensions=ImageSize(640, 640),
     model="dreamshaperxl_lightning_f16.ckpt",
     guidance_scale=2,
@@ -27,7 +27,7 @@ DREAMSHAPER_XL_LIGHTNING = ImageModel(
 
 # Optimized for quality, slow, unfiltered
 AAM_XL_ANIMEMIX_V10 = ImageModel(
-    id="animexl",
+    _id="animexl",
     dimensions=ImageSize(768, 768),
     model="aamxlanimemix_v10_f16.ckpt",
     guidance_scale=2,
@@ -37,7 +37,7 @@ AAM_XL_ANIMEMIX_V10 = ImageModel(
 
 # Optimized for speed and quality, filtered
 JUGGERNAUTXL_V8_RUNDIFFUSION = ImageModel(
-    id="jv8r",
+    _id="jv8r",
     dimensions=ImageSize(768, 768),
     model="juggernautxl_v8rundiffusion_f16.ckpt",
     guidance_scale=7,
@@ -49,7 +49,7 @@ JUGGERNAUTXL_V8_RUNDIFFUSION = ImageModel(
 # Optimized for speed and quality, filtered
 # Configuration based on recommended values by model author
 JUGGERNAUTXL_V9_LIGHTNING = ImageModel(
-    id="jv9l",
+    _id="jv9l",
     dimensions=ImageSize(832, 1216),
     model="juggernautxl_v9rdphoto2lightning_f16.ckpt",
     guidance_scale=2,
@@ -60,7 +60,7 @@ JUGGERNAUTXL_V9_LIGHTNING = ImageModel(
 )
 
 # SDXL_TURBO_V1 = ImageModel(
-#     id="sdxlv1_turbo",
+#     _id="sdxlv1_turbo",
 #     dimensions=Dimensions(768, 768),
 #     model="sdxl_turbo_v1_0_fp16_f16.ckpt",
 #     guidance_scale=2,
@@ -70,7 +70,7 @@ JUGGERNAUTXL_V9_LIGHTNING = ImageModel(
 
 # Optimized for really high quality, filtered
 SDXL_BASE_V1 = ImageModel(
-    id="sdxlv1_base",
+    _id="sdxlv1_base",
     dimensions=ImageSize(1024, 1024),
     model="sd_xl_base_1.0_f16.ckpt",
     guidance_scale=2,
